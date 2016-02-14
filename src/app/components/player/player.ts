867494922516d08a41e99a92402898742faafacc
John Quarles <john.quarles@smilecareclub.com>
2016-02-13 23:07:39 -0600

    color

--- src/app/components/player/player.ts
+++ src/app/components/player/player.ts
@@ -38,6 +38,7 @@ export class Player {
     this.coors_proxy = 'https://crossorigin.me/';
     this.context = new AudioContext();
 
+    // change?
     this.backgroundColor = 'white';
     this.primaryColor = 'black';
     this.secondaryColor = 'black';
